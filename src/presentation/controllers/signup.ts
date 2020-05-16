2eca8d72c22902330fe4d62228a876d911cb9b4a
gianniresende <gianniresende@gmail.com>
2020-05-16 15:47:01 -0300

    feat: move sut creation to a factory helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,26 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
-import { MissingParamErro } from '../errors/missing_param_error'
+import { MissingParamError } from '../errors/missing_param_error'
+import { InvalidParamError } from '../errors/invalid_param_error'
 import { badRequest } from '../helpers/http_helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email_validator'
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamErro(field))
+        return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
